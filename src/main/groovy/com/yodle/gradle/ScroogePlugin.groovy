d21d5884e35923991fd168e94f7bf3b623d9ff2d
David Kesler <dkesler@yodle.com>
2014-06-03 14:21:04 -0400

    Initial checkin of scrooge gradle plugin.  Contains a simple shell plugin that doesn't actually do anything.

---
+++ src/main/groovy/com/yodle/gradle/ScroogePlugin.groovy
@@ -0,0 +1,11 @@
+package com.yodle.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ScroogePlugin implements Plugin<Project> {
+
+  void apply(Project project) {
+
+  }
+}
---
+++ src/test/groovy/com/yodle/gradle/ScroogeGradlePluginApplyTest.groovy
@@ -0,0 +1,24 @@
+package com.yodle.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class ScroogeGradlePluginApplyTest extends Specification {
+
+  Project project
+
+  def setup() {
+    project = ProjectBuilder.builder().build()
+  }
+
+  void "applying scrooge plugin adds plugin"() {
+    when:
+      project.apply plugin: 'scrooge'
+
+    then:
+      project.plugins.hasPlugin(ScroogePlugin)
+  }
+
+}
