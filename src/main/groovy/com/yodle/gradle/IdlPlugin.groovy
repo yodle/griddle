69415965bab9736dbfde094a6aa15ee738a36b9a
David Kesler <DKesler@yodle.com>
2014-06-23 14:48:30 -0400

    Split out IDL plugin from scrooge plugin

---
+++ src/main/groovy/com/yodle/gradle/IdlPlugin.groovy
@@ -0,0 +1,12 @@
+package com.yodle.gradle
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dkesler
+ * Date: 6/23/14
+ * Time: 2:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class IdlPlugin
+{
+}
