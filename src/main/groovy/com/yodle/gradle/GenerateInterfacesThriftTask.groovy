51fc7f487149cb6bda5a0a850e2afb74dd851e20
David Kesler <DKesler@yodle.com>
2014-06-23 17:44:32 -0400

    Fixed problem where thrift generation would fail with more than one input file

--- src/main/groovy/com/yodle/gradle/GenerateInterfacesThriftTask.groovy
+++ src/main/groovy/com/yodle/gradle/GenerateInterfacesThriftTask.groovy
@@ -1,27 +1,48 @@
 package com.yodle.gradle
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.OutputDirectories
+import org.gradle.api.tasks.TaskAction
 
-class GenerateInterfacesThriftTask extends Exec {
+class GenerateInterfacesThriftTask extends DefaultTask {
   @InputFiles FileCollection inputFiles
   @InputFiles FileCollection includedFiles
   @InputFiles FileCollection dependencyFiles
   @OutputDirectories FileCollection outputDirs
   String language
+  String generator = 'thrift'
+  private boolean onWindows = System.properties['os.name'].toLowerCase().contains('windows')
 
-  public configure() {
-    doFirst {
-      args (['-o', outputDirs.getSingleFile(), '--gen', language])
-      args (['-I', dependencyFiles.dir])
-      args (['-I', includedFiles.dir])
-      args inputFiles.files
-      args dependencyFiles.files
+  @TaskAction
+  public void exec() {
+    def args =['-o', outputDirs.getSingleFile(), '--gen', language]
+    args += ['-I', dependencyFiles.dir]
+    args += ['-I', includedFiles.dir]
 
-      executable 'thrift'
+    inputFiles.files.each {
+      generate(it, args)
+    }
+
+    dependencyFiles.files.each {
+      generate(it, args)
+    }
+  }
+
+  def generate(File thriftFile, ArrayList<Serializable> generatorArgs) {
+    if (onWindows) {
+      project.exec {
+        commandLine 'cmd', '/c', generator
+        args generatorArgs + thriftFile.path
+      }
+    } else {
+      project.exec {
+        executable generator
+        args generatorArgs + thriftFile.path
+      }
     }
   }
 }
