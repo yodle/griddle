4cb21574fd6bd13eaeb001493310cca22a5d84f9
David Kesler <DKesler@yodle.com>
2014-06-23 17:52:35 -0400

    added skeleton unit tests for scrooge-java and thrift plugins

---
+++ src/test/groovy/com/yodle/gradle/ScroogeJavaGradlePluginApplyTest.groovy
@@ -0,0 +1,23 @@
+package com.yodle.gradle
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class ScroogeJavaGradlePluginApplyTest extends Specification {
+
+  Project project
+
+  def setup() {
+    project = ProjectBuilder.builder().build()
+  }
+
+  void "applying scrooge-java plugin adds plugin"() {
+    when:
+      project.apply plugin: 'scrooge-java'
+
+    then:
+      project.plugins.hasPlugin(ScroogeJavaPlugin)
+  }
+
+}
